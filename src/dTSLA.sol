b4cc7173a69907ab6a1ffd60f7841a2520379249
SamerAbiWeb3 <samerweb3@yahoo.comgit config --global user.email samerweb3@yahoo.comgit config --global user.email samerweb3@yahoo.com>
2025-02-25 20:16:08 +0200

    init draft

---
+++ src/dTSLA.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity 0.8.25;
+
+contract dTSLA {
+    /// Send an HTTP request to:
+    /// 1. See how much TSLA is bough
+    /// 2. If enough TSLA is in the alpaca(bank) account, if enough, mint dTSLA
+    /// 3. Going to be a 2 transaction function
+    function sendMintRequest() external {}
+}
